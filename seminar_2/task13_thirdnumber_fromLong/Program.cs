12b885f846994cb250feda50eec24e4e8c4ddd0c
Alexey M <baoctos@gmail.com>
2023-02-16 10:00:19 +0600

    add seminar_2 folder with home work

---
+++ seminar_2/task13_thirdnumber_fromLong/Program.cs
@@ -0,0 +1,29 @@
+﻿//int num = 45263; used to check all lengthes
+
+int num = new Random().Next(-99999,100000);
+int number = Math.Abs(num);
+
+System.Console.WriteLine(num);
+
+if(number < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
+else if(number >=100 && number < 1000)
+{
+    int result3 = number % 10;
+    System.Console.WriteLine(result3);
+}
+
+else if(number >=1000 && number < 10000)
+{
+    int result4 = number / 10 % 10;
+    System.Console.WriteLine(result4);
+}
+
+else if(number >=10000 && number < 100000)
+{
+    int result5 = number / 100 % 10;
+    System.Console.WriteLine(result5);
+}
---
+++ seminar_2/task15_daysOfWeek/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите номер дня недели (понедельник-1, вторник-2...): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//int number = new Random().NextBytes(1,8);
+
+if (number == 6 || number == 7)
+{
+    System.Console.WriteLine("Это выходной! Ура!");
+}
+else
+{
+    System.Console.WriteLine("Дуй на работу:)");
+}
