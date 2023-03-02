e5ae521ecd3be5c5898c2d4980032e4b4d0af863
Alexey M <baoctos@gmail.com>
2023-03-02 18:35:54 +0600

    Добавил домашнее задание к 6 семинару

---
+++ seminar_6/task43CrossedLines/Program.cs
@@ -0,0 +1,28 @@
+﻿// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write($"Введите угловой коэффициент k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите угловой коэффициент k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("======================");
+    Console.WriteLine("Эти прямые параллельны");
+    Console.WriteLine("======================");
+}
+else
+{
+    double x = Math.Round((b1 - b2) / (k2 - k1),2);
+    double y = Math.Round(k1 * x + b1,2);
+    Console.WriteLine("========================================");
+    Console.WriteLine($"Координаты точки пересечения:({x};{y})");
+    Console.WriteLine("========================================");
+}
---
+++ seminar_6/task_41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetArray()
+{
+    Console.Write("Сколько чисел вы планируете ввести: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+
+    for (int index = 0; index < size; index++)
+    {
+        Console.Write($"Введите {index + 1} элемент: ");
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("Ваш массив: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}    ");
+    }
+}
+
+int CountPositive(int[] ar)
+{
+    int count = 0;
+
+    for (int ind = 0; ind < ar.Length; ind++)
+    {
+        if (ar[ind] > 0) count+=1;
+    }
+    return count;
+}
+
+int[] mass = GetArray();
+Console.WriteLine();
+PrintArray(mass);
+Console.WriteLine();
+Console.WriteLine($"Количество элементов больше нуля: {CountPositive(mass)}");
